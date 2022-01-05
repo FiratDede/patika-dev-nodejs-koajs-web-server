d91c7a4c08f57ff99f1d7e458531aa2b11c6498a
Fırat Dede <53142518+FiratDede@users.noreply.github.com>
2022-01-06 00:44:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use((ctx,next) => {
+    if(ctx.url==="/"){
+    ctx.response.set("Content-Type","text/html; charset=utf-8");
+     ctx.body = '<h1>Index Sayfasına Hoşgeldiniz</h1>';
+    }
+    else{
+        next();
+        }
+});
+app.use((ctx,next) => {
+    if(ctx.url==="/about"){
+        ctx.response.set("Content-Type","text/html; charset=utf-8");
+        ctx.body = '<h1>About Sayfasına Hoşgeldiniz</h1>';
+    }
+    else{
+    next();
+    }
+});
+app.use((ctx,next) => {
+    if(ctx.url==="/contact"){
+        ctx.response.set("Content-Type","text/html; charset=utf-8");
+        ctx.body = '<h1>Contact Sayfasına Hoşgeldiniz</h1>';
+    }
+    else{
+        next();
+        }
+});
+app.use((ctx,next) => {
+    
+  ctx.status=404; 
+  ctx.body="404 Page Not Found"
+ });
+
+
+app.listen(3000);
